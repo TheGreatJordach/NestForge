25d6aa0c470394a14ff89323ceaaa4460cc280a6
Jordach Makaya <4thinkai@gmail.com>
2025-03-09 04:36:08 +0000

    config .env

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggingService } from './core/logging/logging.service';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    bufferLogs: true, // Buffer logs until Winston is ready
-  });
+  try {
+    const app = await NestFactory.create(AppModule, {
+      bufferLogs: true, // Buffer logs until Winston is ready
+    });
 
-  const logger = new LoggingService();
-  app.useLogger(logger);
+    const logger = new LoggingService();
+    app.useLogger(logger);
 
-  await app.listen(process.env.PORT ?? 3000);
+    const configService = app.get(ConfigService);
+    const port: number = configService.getOrThrow<number>('APP_PORT') ?? 3000;
+
+    await app.listen(port);
+    logger.log(`🚀 App is running on port ${port}`);
+  } catch (error) {
+    console.error('🛑 Logging system failed!', error);
+    process.exit(1); // ✅ Stops app to avoid a broken state
+  }
 }
 bootstrap();
